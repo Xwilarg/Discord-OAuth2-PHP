a0cbeb0dda6153b9aee3a836df6258a91b5448e4
Christian Chaux <xwilarg@yahoo.fr>
2020-03-10 00:37:06 +0100

    Change example

--- sample/index.php
+++ sample/index.php
@@ -8,7 +8,7 @@ use Xwilarg\Discord\OAuth2;
 // clientId: Client ID of the application
 // secret: Secret of the application
 // url: The redirect URL (URL called after the user is logged in, must be registered in https://discordapp.com/developers/applications/[YourAppId]/oauth)
-$auth = json_decode(file_get_contents('token.json'), true);
+$auth = json_decode(file_get_contents('Credentials.json'), true);
 $oauth2 = new OAuth2($auth["clientId"], $auth["secret"], $auth["url"]);
 
 if ($oauth2->isRedirected() === false) { // Did the client already logged in ?
